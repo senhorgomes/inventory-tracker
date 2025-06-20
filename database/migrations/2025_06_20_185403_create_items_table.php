ea832fc22bc520db81d80ee53796a18321e3ff30
Bryan Gomes <bryanpgomes@gmail.com>
2025-06-20 15:04:23 -0400

    Create schema for items table

---
+++ database/migrations/2025_06_20_185403_create_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('description')->nullable();
+            $table->bigInteger('stock')->default(0);
+            $table->bigInteger('price');
+            $table->bigInteger('sku');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
