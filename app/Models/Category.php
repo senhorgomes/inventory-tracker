b705d8bc356d49ba29af8e03170c0bdc250fc91f
Bryan Gomes <bryanpgomes@gmail.com>
2025-06-23 15:12:15 -0400

    Create empty seeders

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,4 +13,9 @@ class Category extends Model
         'name',
         'description',
     ];
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -18,4 +18,9 @@ class Item extends Model
         'sku',
         'category_id',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
