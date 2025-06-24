0dc1e6fbb80c8fdb0de902ba59cb5573ff9f9b2b
Bryan Gomes <bryanpgomes@gmail.com>
2025-06-24 14:57:58 -0400

    Fix ItemController bug, missing import for ItemModel

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Item;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ItemController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('dashboard', function () {
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
 require __DIR__.'/category.php';
+require __DIR__.'/item.php';
