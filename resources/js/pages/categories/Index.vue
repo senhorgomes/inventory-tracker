9293a0499dd967335905dbe2fe304f32ddab772e
Bryan Gomes <bryanpgomes@gmail.com>
2025-06-24 00:02:39 -0400

    Create index page for categories:

---
+++ resources/js/pages/categories/Index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { Category } from '@/types';
+
+interface Props {
+    categories: Category[];
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <table class="w-full table-auto">
+        <thead>
+            <tr>
+                <th class="px-4 py-2">Name</th>
+                <th class="px-4 py-2">Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="category in categories" :key="category.id" class="bg-white border-b">
+                <td class="px-4 py-2">{{ category.name }}</td>
+                <td class="px-4 py-2">{{ category.description }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
