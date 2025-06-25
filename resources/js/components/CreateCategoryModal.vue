920b2a92f7a5360d189216c3f6187fbf4c921332
Bryan Gomes <bryanpgomes@gmail.com>
2025-06-25 19:52:57 -0400

    Create Category Modal

---
+++ resources/js/components/CreateCategoryModal.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Create Category</h2>
+        <input v-model="name" placeholder="Category Name" />
+        <input v-model="description" placeholder="Category Description" />
+        <button @click="createCategory">Create</button>
+        <button @click="emit('cancel')">Cancel</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const name = ref('');
+const description = ref('');
+
+const emit = defineEmits(['create', 'cancel']);
+
+const createCategory = () => {
+    emit('create', { name: name.value, description: description.value });
+};
+</script>
